258116915bf6231b46a647b98aacdaa0644cb8dd
Giannis Hatziioannidis <153095545+giannisha@users.noreply.github.com>
2024-01-31 13:02:46 +0200

    Update deploy-infra.ps1

--- src/ops/deploy-infra.ps1
+++ src/ops/deploy-infra.ps1
@@ -1,2 +1,2 @@
 # az login
-az deployment sub create --location $env:LOCATION --resourceGroupName $env:CLOUDBANK_RESOURCE_GROUP --template-file src/iac/main.bicep  --name cloudbank-simple-iac
+az deployment sub create --location $env:LOCATION --template-file src/iac/main.bicep  --name cloudbank-simple-iac --parameters '{ \"resourceGroupName\": \"cloudbank-simple-rg\" }'
